6f41fb700936bbe76d94338535fa2f2bf044b6d5
Triston <t_claydon@hotmail.com>
2021-06-29 21:30:23 +0100

    first commit

---
+++ src/test/java/SoftmaxSolutions/ImageFilter01/ImageFilter01ApplicationTests.java
@@ -0,0 +1,13 @@
+package SoftmaxSolutions.ImageFilter01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageFilter01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
